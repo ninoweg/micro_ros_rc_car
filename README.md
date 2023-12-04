e1c4c86f922de3373d223f252efbb1e285062ebb
Nino Wegleitner <e1644401@student.tuwien.ac.at>
2023-12-04 19:24:51 +0100

    update to micro-ROS

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
-# rc_car
-The code and instructions found in this repository outline the process of converting an RC car into a mobile platform that can be easily controlled through ROS. Key hardware elements involved in this conversion include a 1/10 scale RC car, a Raspberry Pi 4 8GB acting as the high-level board computer, and an Arduino Mega 2560 utilized as the VCU.
+# micro_ros_rc_receiver
+This projects outlines the process of building a ROS 2 RC receiver using ESP32 and micro-ROS. It aims to design a ROS 2 plug-and-play receiver which can be used to replace any standard RC receiver. The receiver can communicate either over serial using USB-C or Wifi.
 
-``` 
-rosrun rosserial_python serial_node.py /dev/ttyACM0
+## Hardware 
+### Basic Setup
+The receiver is based on a [Adafruit ESP32 Feather V2](https://www.adafruit.com/product/5400) (any board supported by [micro_ros_arduino](https://github.com/micro-ROS/micro_ros_arduino#supported-boards) can be used). In the future, the Adafruit ESP32 Feather V2 might be replaced by the [Arduino Nano RP2040 Connect](https://docs.arduino.cc/hardware/nano-rp2040-connect) to make use of the additional 6-axis IMU. 
+
+A custom PCB is used to connect at least two (more can be added) 3-pin plugs for connecting e.g. the ESC and servo of a standard RC car. This can be built using cheap perfboards. 
+
+This simple hardware setup already allows us to control a RC car using ROS 2. 
+
+### Advanced Setup
+For the advanced setup the receiver includes ports for four [hall sensors](https://cdn-reichelt.de/documents/datenblatt/B400/DATASHEET_SERIE_SS400.pdf) which are used for wheel odometry.
+
+## Software
+### Setup
+To integrate the micro-ROS receiver into a ROS 2 environment, two things are needed: a micro-ROS agent running on a host computer and the micro-ROS client running on the ESP32. 
+
+#### micro-ROS agent
+To get the agent running, simply setup [Docker](https://www.docker.com/) on your host computer and run the following command in a shell:
 ```
+# Serial micro-ROS Agent
+docker run -it --rm -v /dev:/dev --privileged --net=host microros/micro-ros-agent:[YOUR ROS 2 VERSION] serial --dev [YOUR BOARD PORT] -v6
+``` 
+This command connects to an micro-ROS client over serial. Check your PORT and ROS 2 VERSION.
+Find more information [here](https://hub.docker.com/r/microros/esp-idf-microros/). 
+There is also an command for connecting the client over Wifi.
 
-## hardware list
-* TRAXXAS Slash schwarz RTR 1/10 2WD Short Course Racing Truck
-* Raspberry Pi 4 8GB + USB to M.2 Adapter + SSD 
-* Arduino Mega 2560 + custom shield
-* Luxonis OAK-D
-* hall sensor based odometry
-* LIPO battery 14.8V + DC/DC to 5V USB-C for power supply of Raspberry and OAK-D
-* LIPO battery 7.4V for RC car
-* Multiplexers for battery meter: https://cdn-reichelt.de/documents/datenblatt/A200/LS153_REN.pdf
-* Bipolar hall sensor for odometry: https://cdn-reichelt.de/documents/datenblatt/B400/DATASHEET_SERIE_SS400.pdf
+When using WSL2 this [article](https://learn.microsoft.com/en-us/windows/wsl/connect-usb) discribes how to connect a USB device to the Linux distribution. 
 
+#### micro-ROS client
+The client is based on [micro-ROS for Arduino](https://github.com/micro-ROS/micro_ros_arduino/tree/humble) reconnection [example](https://github.com/micro-ROS/micro_ros_arduino/blob/humble/examples/micro-ros_reconnection_example/micro-ros_reconnection_example.ino). Install Arduino IDE 2, add [Arduino-ESP32 support](https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/installing.html) and download the micro-ROS library for Arduino following the instructions on [micro-ROS for Arduino](https://github.com/micro-ROS/micro_ros_arduino/tree/humble#how-to-use-the-precompiled-library). Use the board manage to select your board and port.
+
+Clone this repo and upload the micro_ros_rc_receiver.ino file to your device.
+
+Connect the device over USB to the host where the micro-ROS agent is running.
+To check the devices port run `ls /dev/ttyACM*` or `ls /dev/ttyUSB*` (linux) or check your device manager (windows). 
---
+++ micro_ros_rc_receiver.ino
@@ -0,0 +1,167 @@
+#include <micro_ros_arduino.h>
+
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rmw_microros/rmw_microros.h>
+
+#include <std_msgs/msg/int32.h>
+#include <geometry_msgs/msg/twist.h>
+
+#include <ESP32Servo.h>
+
+#define LED_PIN 13
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return false;}}
+#define EXECUTE_EVERY_N_MS(MS, X)  do { \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+} while (0)\
+
+rclc_support_t support;
+rcl_node_t node;
+rcl_timer_t timer;
+rclc_executor_t executor;
+rcl_allocator_t allocator;
+rcl_publisher_t publisher;
+rcl_subscription_t subscriber;
+bool micro_ros_init_successful;
+
+geometry_msgs__msg__Twist twist_msg;
+std_msgs__msg__Int32 msg;
+
+Servo accel; 
+Servo steer; 
+
+enum states {
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+} state;
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+  (void) last_call_time;
+  if (timer != NULL) {
+    rcl_publish(&publisher, &msg, NULL);
+    msg.data++;
+  }
+}
+
+//twist message cb
+void subscription_callback(const void *msgin) {
+  const geometry_msgs__msg__Twist * twist_msg = (const geometry_msgs__msg__Twist *)msgin;
+  // if velocity in x direction is 0 turn off LED, if 1 turn on LED
+  accel.write(1500 + 1000 * twist_msg->linear.x);
+  steer.write(1500 - 1000 * twist_msg->angular.z);
+}
+
+// Functions create_entities and destroy_entities can take several seconds.
+// In order to reduce this rebuild the library with
+// - RMW_UXRCE_ENTITY_CREATION_DESTROY_TIMEOUT=0
+// - UCLIENT_MAX_SESSION_CONNECTION_ATTEMPTS=3
+
+bool create_entities()
+{
+  allocator = rcl_get_default_allocator();
+
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "vcu", "", &support));
+
+  // create subscriber
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "vcu_twist_subscriber"));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_best_effort(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "vcu_int_counter"));
+
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &twist_msg, &subscription_callback, ON_NEW_DATA));
+
+  return true;
+}
+
+void destroy_entities()
+{
+  rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+  (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+  rcl_publisher_fini(&publisher, &node);
+  rcl_timer_fini(&timer);
+  rcl_subscription_fini(&subscriber, &node);
+  rclc_executor_fini(&executor);
+  rcl_node_fini(&node);
+  rclc_support_fini(&support);
+}
+
+void setup() {
+  set_microros_transports();
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);  
+  
+  delay(1000);
+
+  steer.attach(27);
+  accel.attach(33);
+
+  state = WAITING_AGENT;
+
+  // msg.data = 0;
+
+}
+
+void loop() {
+  switch (state) {
+    case WAITING_AGENT:
+      EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
+      break;
+    case AGENT_AVAILABLE:
+      state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
+      if (state == WAITING_AGENT) {
+        destroy_entities();
+      };
+      break;
+    case AGENT_CONNECTED:
+      EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
+      if (state == AGENT_CONNECTED) {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+      }
+      break;
+    case AGENT_DISCONNECTED:
+      destroy_entities();
+      state = WAITING_AGENT;
+      break;
+    default:
+      break;
+  }
+
+  if (state == AGENT_CONNECTED) {
+    digitalWrite(LED_PIN, 1);
+  } else {
+    digitalWrite(LED_PIN, 0);
+  }
+}
