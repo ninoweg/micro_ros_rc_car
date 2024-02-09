35c6218bd345bd7dbd6480996522c414cf9f15d2
Nino Wegleitner <nino.wegleitner@gmx.at>
2024-02-09 22:37:13 +0100

    update

--- micro_ros_rc_receiver.ino
+++ micro_ros_rc_receiver.ino
@@ -8,7 +8,7 @@
 #include <rmw_microros/rmw_microros.h>
 
 #include <std_msgs/msg/int32.h>
-#include <geometry_msgs/msg/twist.h>
+#include <std_msgs/msg/int16.h>
 
 #include <ESP32Servo.h>
 
@@ -26,14 +26,20 @@ rcl_timer_t timer;
 rclc_executor_t executor;
 rcl_allocator_t allocator;
 rcl_publisher_t publisher;
-rcl_subscription_t subscriber;
+rcl_subscription_t subscriber1;
+rcl_subscription_t subscriber2;
 bool micro_ros_init_successful;
 
-geometry_msgs__msg__Twist twist_msg;
 std_msgs__msg__Int32 msg;
 
-Servo accel; 
-Servo steer; 
+std_msgs__msg__Int16 channel1_msg;
+std_msgs__msg__Int16 channel2_msg;
+
+Servo channel1; 
+Servo channel2;
+
+bool channel1_wd;
+bool channel2_wd;
 
 enum states {
   WAITING_AGENT,
@@ -42,21 +48,37 @@ enum states {
   AGENT_DISCONNECTED
 } state;
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+void wd_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
   (void) last_call_time;
   if (timer != NULL) {
     rcl_publish(&publisher, &msg, NULL);
     msg.data++;
   }
+  if(!channel1_wd) {
+    channel1.write(1500);
+  }
+  if(!channel2_wd) {
+    channel2.write(1500);
+  }
+  channel1_wd = false;
+  channel2_wd = false;
 }
 
-//twist message cb
-void subscription_callback(const void *msgin) {
-  const geometry_msgs__msg__Twist * twist_msg = (const geometry_msgs__msg__Twist *)msgin;
+//channel1 message cb
+void channel1_callback(const void *msgin) {
+  const std_msgs__msg__Int16 * msg = (const std_msgs__msg__Int16 *)msgin;
   // if velocity in x direction is 0 turn off LED, if 1 turn on LED
-  accel.write(1500 + 1000 * twist_msg->linear.x);
-  steer.write(1500 - 1000 * twist_msg->angular.z);
+  channel1.write(msg->data);
+  channel1_wd = true;
+}
+
+//channel2 message cb
+void channel2_callback(const void *msgin) {
+  const std_msgs__msg__Int16 * msg = (const std_msgs__msg__Int16 *)msgin;
+  // if velocity in x direction is 0 turn off LED, if 1 turn on LED
+  channel2.write(msg->data);
+  channel2_wd = true;
 }
 
 // Functions create_entities and destroy_entities can take several seconds.
@@ -72,35 +94,43 @@ bool create_entities()
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
   // create node
-  RCCHECK(rclc_node_init_default(&node, "vcu", "", &support));
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_rc_receiver", "", &support));
 
-  // create subscriber
+  // create subscriber for channel1
   RCCHECK(rclc_subscription_init_default(
-    &subscriber,
+    &subscriber1,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
-    "vcu_twist_subscriber"));
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+    "channel1"));
 
-  // create publisher
+  // create subscriber for channel2
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber2,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16),
+    "channel2"));
+
+  // create publisher (just for testing)
   RCCHECK(rclc_publisher_init_best_effort(
     &publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    "vcu_int_counter"));
+    "counter"));
 
   // create timer,
-  const unsigned int timer_timeout = 1000;
+  const unsigned int timer_timeout = 500;
   RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
     RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
+    wd_timer_callback));
 
   // create executor
   executor = rclc_executor_get_zero_initialized_executor();
-  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
-  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &twist_msg, &subscription_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber1, &channel1_msg, &channel1_callback, ON_NEW_DATA));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber2, &channel2_msg, &channel2_callback, ON_NEW_DATA));
 
   return true;
 }
@@ -112,7 +142,8 @@ void destroy_entities()
 
   rcl_publisher_fini(&publisher, &node);
   rcl_timer_fini(&timer);
-  rcl_subscription_fini(&subscriber, &node);
+  rcl_subscription_fini(&subscriber1, &node);
+  rcl_subscription_fini(&subscriber2, &node);
   rclc_executor_fini(&executor);
   rcl_node_fini(&node);
   rclc_support_fini(&support);
@@ -125,12 +156,16 @@ void setup() {
   
   delay(1000);
 
-  steer.attach(27);
-  accel.attach(33);
+  channel1.attach(27);
+  channel2.attach(33);
 
   state = WAITING_AGENT;
 
-  // msg.data = 0;
+  channel1.write(1500);
+  channel2.write(1500);
+
+  channel1_wd = false;
+  channel2_wd = false;
 
 }
 
